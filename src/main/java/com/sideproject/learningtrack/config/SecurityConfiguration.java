081769e9c0fb63be83a6b835637023e1b0091e70
Tuan Pham <luke.ken1993@gmail.com>
2021-04-06 15:13:19 +0200

    Update cors config

--- src/main/java/com/sideproject/learningtrack/config/SecurityConfiguration.java
+++ src/main/java/com/sideproject/learningtrack/config/SecurityConfiguration.java
@@ -7,11 +7,12 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+            .cors().and()
             .csrf().disable()
             .authorizeRequests().anyRequest().permitAll();
     }
